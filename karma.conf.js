8981559b8d4a7ce7087f393b8a6f98182e827bbf
Marc Kassay <marckassay@gmail.com>
2019-08-18 18:55:41 -0400

    build(.): move repo to new Angular 8 lib context

--- karma.conf.js
+++ karma.conf.js
@@ -1,91 +1,32 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'karma-typescript'],
-
-    // list of files / patterns to load in the browser
-    files: [
-      // Required by PhantomJS. Otherwise strange errors will be thrown.
-      'node_modules/traceur/bin/traceur-runtime.js',
-
-      { pattern: "base.spec.ts" },
-      // { pattern: "src/app/**/*.+(ts|html)" }
-      { pattern: 'src/**/*.ts' }
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-
-    // list of files to exclude
-    exclude: ['src/**/*.d.ts', 'src/**/*.ngfactory.ts'],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      '**/*.ts': ['karma-typescript']
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'karma-typescript'],
-
-    // web server port
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../../coverage/angular-coordinates'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // TODO: Create another config file with all browsers.
-    // TODO: IE and Edge
-    browsers: process.env.TRAVIS ? ['PhantomJS', 'Chrome_travis_ci'] : [
-      'PhantomJS',
-      // 'Chrome',
-      // 'ChromeCanary',
-      // 'Chrome_small'
-      // 'Firefox',
-      // 'FirefoxDeveloper',
-      // 'Safari',
-      // 'Opera',
-      // 'IE'
-    ],
-
-    customLaunchers: {
-      Chrome_small: {
-        base: 'ChromeCanary',
-        flags: [
-          '--window-size=400,400',
-          '--window-position=-400,0'
-        ]
-      },
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    browsers: ['Chrome'],
     singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-
-    karmaTypescriptConfig: {
-      reports: {
-        "html": "coverage",
-        "text": "",
-        "lcovonly": ""
-      }
-    }
-  })
+    restartOnFileChange: true
+  });
 };
---
+++ src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-coordinates
+ */
+
+export * from './lib/angular-coordinates.service';
+export * from './lib/angular-coordinates.component';
+export * from './lib/angular-coordinates.module';
